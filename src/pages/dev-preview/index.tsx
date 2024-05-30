0ba73b166da4d032a4180345180eb659e3da306a
striker1826 <alstjq123579@daum.net>
2024-05-30 17:02:02 +0900

    styled-component nextjs 설정

--- src/pages/dev-preview/index.tsx
+++ src/pages/dev-preview/index.tsx
@@ -1,13 +1,23 @@
+import theme from '@/styles/theme';
 import React from 'react';
+import styled from 'styled-components';
+
+const ExampleBtn = styled.button`
+  width: 100px;
+  height: 50px;
+  background-color: ${theme.Colors.Red[10]};
+`;
 
 const index = () => {
-  <>
-    <div>asdsa</div>
-    <hr />
-    <div>asdas</div>
-    <hr />
-    <div>asdas</div>
-  </>;
+  return (
+    <>
+      <ExampleBtn>asdsa</ExampleBtn>
+      <hr />
+      <div>asdas</div>
+      <hr />
+      <div>asdas</div>
+    </>
+  );
 };
 
 export default index;
