df45a2e857e3a94253539fc146a6a9688395daf1
Batrnan <tjdwo2323@naver.com>
2024-05-30 19:38:21 +0900

    임시

---
+++ src/shared/components/NotificationModal/NotificationModal.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import * as S from './notificationModal.styled';
+
+const NotificationModal: React.FC = () => {
+  return <S.ModalContainer>ㅎㅇ</S.ModalContainer>;
+};
+
+export default NotificationModal;
---
+++ src/shared/components/NotificationModal/notificationModal.styled.ts
@@ -0,0 +1,9 @@
+import theme from '@/styles/theme';
+import styled from 'styled-components';
+
+export const ModalContainer = styled.div`
+  width: 33px;
+  height: 380px;
+  padding: 24px 20px;
+  background-color: ${theme.Colors.Red[20]};
+`;
