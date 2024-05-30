25db4f08fa7ea5ee0c76d14c00c9010d1956a13c
Kimhansaem <khs_38@naver.com>
2024-05-30 21:30:34 +0900

    feat: 테이블 > badge 컴포넌트 추가

---
+++ src/shared/components/Table/Badge/badge.styled.ts
@@ -0,0 +1,30 @@
+import theme from '@/styles/theme';
+import { styled } from 'styled-components';
+
+export interface BadgeStyled {
+  $status: '거절' | '대기중' | '승인 완료';
+}
+const config = {
+  거절: { background: theme.Colors.Red[10], color: theme.Colors.Red[40] },
+  대기중: { background: theme.Colors.Green[10], color: theme.Colors.Green[20] },
+  '승인 완료': {
+    background: theme.Colors.Blue[10],
+    color: theme.Colors.Blue[20],
+  },
+};
+
+export const Badge = styled.span<BadgeStyled>`
+  text-align: center;
+  padding: 6px 10px;
+  width: 75px;
+  font-size: 14px;
+
+  border-radius: 20px;
+  color: ${props => config[props.$status]['color']};
+  background: ${props => config[props.$status]['background']};
+
+  @media (max-width: 744px) {
+    font-size: 12px;
+    width: 67px;
+  }
+`;
