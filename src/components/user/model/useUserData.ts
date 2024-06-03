af866653d5c4a52aa8480295c247c0e9b203da4c
Kimhansaem <khs_38@naver.com>
2024-06-03 21:18:10 +0900

    feat: mypage > usequery 이용 조회 추가

---
+++ src/components/user/model/useUserData.ts
@@ -0,0 +1,16 @@
+import { useUserData } from '@/shared/store/useUserData';
+import { apiInstance } from '@/shared/utils/axios';
+import { getUserApi } from '@/widgets/mystore/api/getUser.api';
+import { useQuery } from '@tanstack/react-query';
+
+export const useUserQuery = () => {
+  const { user_id } = useUserData();
+  const { data, isError, isLoading } = useQuery({
+    queryKey: [`/users/${user_id}`],
+    queryFn: () => {
+      return getUserApi(user_id);
+    },
+    enabled: !!user_id,
+  });
+  return { data, isError, isLoading };
+};
--- src/pages/mypage/index.tsx
+++ src/pages/mypage/index.tsx
@@ -3,15 +3,18 @@ import * as S from './index.styled';
 import { AddProfile } from '@/widgets/mypage/ul/addProfile/AddProfile';
 import Header from '@/shared/components/Header/Header';
 import Footer from '@/shared/components/Footer/Footer';
+import { useUserQuery } from '@/components/user/model/useUserData';
 
 const MyPage = () => {
+  const { data, isError, isLoading } = useUserQuery();
+  const content = data?.data.item.phone ? null : <AddProfile />;
   return (
     <>
       <Header />
       <S.Container>
         <S.ContentWrap>
           <S.MyPageHeader>내 프로필</S.MyPageHeader>
-          <AddProfile />
+          {content}
         </S.ContentWrap>
       </S.Container>
       <Footer />
