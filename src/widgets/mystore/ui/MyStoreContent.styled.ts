f46d8fbd3d2f7c255aa1eb09aff5616da81fb1ed
striker1826 <alstjq123579@daum.net>
2024-06-03 17:12:05 +0900

    zustand LoaclStorage에 저장 & myStore 상세보기 페이지 작업 중

---
+++ src/widgets/mystore/ui/MyStoreContent.styled.ts
@@ -0,0 +1,27 @@
+import styled from 'styled-components';
+
+export const Description = styled.p`
+  color: var(--The-julge-black, #111322);
+  font-size: 16px;
+  font-weight: 400;
+  line-height: 26px; /* 162.5% */
+`;
+
+export const MyStoreContent = styled.div`
+  width: 965px;
+  border-radius: 12px;
+  border: 1px solid var(--The-julge-gray-20, #e5e4e7);
+  padding: 60px 24px;
+  gap: 24px;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+`;
+
+export const ButtonWrap = styled.div`
+  width: 346px;
+`;
+
+// export const Title = styled.h1`
+// `
