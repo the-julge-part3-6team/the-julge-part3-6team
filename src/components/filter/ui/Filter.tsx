5fe952cb1a73a3c133fd0c6649e8c117bd85a531
Batrnan <tjdwo2323@naver.com>
2024-06-06 23:33:17 +0900

    Fix: zustand 사용, filter 컴포넌트 분리

--- src/components/filter/ui/Filter.tsx
+++ src/components/filter/ui/Filter.tsx
@@ -1,14 +1,11 @@
 import * as S from './Filter.styled';
-import Image from 'next/image';
-import closeImg from '@/assets/close.svg';
-import { useState } from 'react';
 import { useModal } from '@/shared/store/useModal';
-import DatePicker from 'react-datepicker';
 import { FilterState } from '@/types/filterState';
-import { locations } from '../constant/locations';
-import CustomButton from '@/shared/components/Button/CustomButton/CustomButton';
-import RedButton from '@/shared/components/Button/RedButton/RedButton';
-import Input from '@/shared/components/Input/Input';
+import { useFilterStore } from '@/shared/store/useFilterStore';
+import FilterLocation from './FilterLocation/FilterLocation';
+import FilterDate from './FilterDate/FilterDate';
+import FilterPrice from './FilterPrice/FilterPrice';
+import FilterButton from './FilterButton/FilterButton';
 
 interface Props {
   modalKey: string;
@@ -19,40 +16,21 @@ const Filter = ({ modalKey, onApply }: Props) => {
   const { isOpen, key } = useModal();
   const isSelected = key === modalKey;
 
-  const [startDate, setStartDate] = useState<Date | null>(null);
-  // const [price, setPrice] = useState<number>(0);
-  const [selectedLocations, setSelectedLocations] = useState<string[]>([]);
-
-  const onChangeStartDate = (date: Date | null) => {
-    setStartDate(date);
-  };
-
-  // const onChangePrice = (e: React.ChangeEvent<HTMLInputElement>) => {
-  //   setPrice(Number(e.target.value));
-  // };
-
-  const toggleLocation = (location: string) => {
-    setSelectedLocations(prev =>
-      prev.includes(location)
-        ? prev.filter(loc => loc !== location)
-        : [...prev, location],
-    );
-  };
-
-  const removeSelectedLocation = (selectedLocation: string) => {
-    setSelectedLocations(prev => prev.filter(loc => loc !== selectedLocation));
-  };
-
-  const resetFilters = () => {
-    setStartDate(null);
-    // setPrice(0);
-    setSelectedLocations([]);
-  };
+  const {
+    startDate,
+    setStartDate,
+    price,
+    setPrice,
+    selectedLocations,
+    toggleLocation,
+    removeLocation,
+    resetFilters,
+  } = useFilterStore();
 
   const applyFilters = () => {
     const filters: FilterState = {
       startDate,
-      // price,
+      price,
       selectedLocations,
     };
     onApply(filters);
@@ -63,80 +41,21 @@ const Filter = ({ modalKey, onApply }: Props) => {
       {isOpen && isSelected && (
         <S.FilterContainer>
           <S.Header>상세 필터</S.Header>
-          <S.Section>
-            <S.SectionTitle>위치</S.SectionTitle>
-            <S.LocationList>
-              {locations.map((location, index) => (
-                <S.LocationItem
-                  key={index}
-                  onClick={() => toggleLocation(location)}
-                >
-                  {location}
-                </S.LocationItem>
-              ))}
-            </S.LocationList>
-            <S.SelectedLocation>
-              {selectedLocations.map((selectedLocation, index) => (
-                <S.Tag
-                  key={index}
-                  onClick={() => removeSelectedLocation(selectedLocation)}
-                >
-                  {selectedLocation} <Image src={closeImg} alt="닫기 버튼" />
-                </S.Tag>
-              ))}
-            </S.SelectedLocation>
-          </S.Section>
+          <S.SectionTitle>위치</S.SectionTitle>
+          <FilterLocation
+            selectedLocations={selectedLocations}
+            toggleLocation={toggleLocation}
+            removeLocation={removeLocation}
+          />
           <S.Divider />
-          <S.Section>
-            <S.SectionTitle>시작일</S.SectionTitle>
-            <S.CustomDatePickerWrapper>
-              <DatePicker
-                selected={startDate}
-                onChange={onChangeStartDate}
-                placeholderText="입력"
-                dateFormat="yyyy.MM.dd"
-              />
-            </S.CustomDatePickerWrapper>
-          </S.Section>
+          <S.SectionTitle>시작일</S.SectionTitle>
+          <FilterDate startDate={startDate} setStartDate={setStartDate} />
           <S.Divider />
-          <S.Section>
-            {/* <S.SectionTitle>금액</S.SectionTitle> */}
-            {/* <S.PriceWrapper>
-              <S.PriceInputWrapper>
-                <S.PriceInput
-                  value={price}
-                  placeholder="입력"
-                  type="number"
-                  onChange={onChangePrice}
-                />
-                <S.Currency>원</S.Currency>
-              </S.PriceInputWrapper>
-              <S.PriceText>이상부터</S.PriceText>
-            </S.PriceWrapper> */}
-            <S.PriceWrapper>
-              <S.PriceInputWrapper>
-                <Input
-                  inputType="text"
-                  label="금액"
-                  type="hourlyWage"
-                  placeholder="입력"
-                />
-              </S.PriceInputWrapper>
-              <S.PriceText>이상부터</S.PriceText>
-            </S.PriceWrapper>
-          </S.Section>
-          <S.ButtonContainer>
-            <S.ResetButton>
-              <CustomButton
-                text="초기화"
-                color="#EA3C12"
-                onClick={resetFilters}
-              />
-            </S.ResetButton>
-            <S.ApplyButton>
-              <RedButton text="적용하기" onClick={applyFilters} />
-            </S.ApplyButton>
-          </S.ButtonContainer>
+          <FilterPrice setPrice={setPrice} />
+          <FilterButton
+            resetFilters={resetFilters}
+            applyFilters={applyFilters}
+          />
         </S.FilterContainer>
       )}
     </>
--- src/components/filter/ui/FilterPrice/FilterPrice.tsx
+++ src/components/filter/ui/FilterPrice/FilterPrice.tsx
@@ -1,3 +1,25 @@
-const FilterPrice = () => {};
+import * as S from './FilterPrice.styled';
+import Input from '@/shared/components/Input/Input';
+
+interface Props {
+  setPrice: (price: number) => void;
+}
+
+const FilterPrice = ({ setPrice }: Props) => {
+  return (
+    <S.PriceWrapper>
+      <S.PriceInputWrapper>
+        <Input
+          inputType="text"
+          label="금액"
+          type="hourlyWage"
+          placeholder="입력"
+          onChange={e => setPrice(Number(e.target.value))}
+        />
+      </S.PriceInputWrapper>
+      <S.PriceText>이상부터</S.PriceText>
+    </S.PriceWrapper>
+  );
+};
 
 export default FilterPrice;
