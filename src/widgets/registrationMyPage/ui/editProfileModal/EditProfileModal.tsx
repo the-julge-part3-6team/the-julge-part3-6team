14e9f63809280baa375c89e15302fb7ddb670bb0
Kimhansaem <khs_38@naver.com>
2024-06-07 21:52:20 +0900

    remove: modal bg 이벤트 삭제

--- src/widgets/registrationMyPage/ui/editProfileModal/EditProfileModal.tsx
+++ src/widgets/registrationMyPage/ui/editProfileModal/EditProfileModal.tsx
@@ -1,32 +1,34 @@
 import RedButton from '@/shared/components/Button/RedButton/RedButton';
 import Modal from '@/shared/components/Modal/Modal';
+import { useModal } from '@/shared/store/useModal';
 import { useRouter } from 'next/router';
 import React from 'react';
 
 export const EditProfileModal = () => {
   const router = useRouter();
-
-  const onClickCloseModal = () => {
-    router.push('/mypage');
-  };
+  const { setIsClose } = useModal();
 
   return (
-    <div onClick={onClickCloseModal}>
-      <Modal
-        modalKey="등록완료"
-        modalHeader={
-          <>
-            <p>등록이 완료 되었습니다.</p>
-          </>
-        }
-        modalFooter={
-          <>
-            <div style={{ width: '80px' }}>
-              <RedButton onClick={() => router.push('/mypage')} text="확인" />
-            </div>
-          </>
-        }
-      />
-    </div>
+    <Modal
+      modalKey="등록완료"
+      modalHeader={
+        <>
+          <p>등록이 완료 되었습니다.</p>
+        </>
+      }
+      modalFooter={
+        <>
+          <div style={{ width: '80px' }}>
+            <RedButton
+              onClick={() => {
+                router.push('/mypage');
+                setIsClose();
+              }}
+              text="확인"
+            />
+          </div>
+        </>
+      }
+    />
   );
 };
