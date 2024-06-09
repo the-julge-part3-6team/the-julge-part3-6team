2f0157a5cc9b3b4c408ae28bc6fab0e1ed2f403f
eqypo9 <eqypo@naver.com>
2024-06-10 02:03:33 +0900

    feat: 로컬 스토리지로 최신 6개 공고 보이기 로직 추가

--- src/pages/noticedetail/index.tsx
+++ src/pages/noticedetail/index.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import Image from 'next/image';
 import * as S from './index.styled';
 import { useRouter } from 'next/router';
@@ -7,28 +7,43 @@ import Header from '@/shared/components/Header/Header';
 import Footer from '@/shared/components/Footer/Footer';
 import PostImage from '@/shared/components/PostList/PostImage/PostImage';
 import PostInform from '@/shared/components/PostList/PostInform/PostInform';
-import PostPrice from '@/shared/components/PostList/PostPrice/PostPrice';
 import Post from '@/shared/components/PostList/Post/Post';
-import { useUserQuery } from '@/components/user/model/useUserData';
 import Modal from '@/shared/components/Modal/Modal';
-import { useModal } from '@/shared/store/useModal';
 import CustomButton from '@/shared/components/Button/CustomButton/CustomButton';
 import RedButton from '@/shared/components/Button/RedButton/RedButton';
-// import { recentPosts } from './data/recentPosts'; 목업 데이터
 import storeImg from '@/assets/store.png';
 import cautionImg from '@/assets/caution.svg';
 import checkImg from '@/assets/check.svg';
+import { useModal } from '@/shared/store/useModal';
+import { useUserQuery } from '@/components/user/model/useUserData';
 
 const NoticeDetail = () => {
   const router = useRouter();
   const searchParams = useSearchParams();
   const shop_id = searchParams.get('shop_id');
-  const { data, isError, isLoading } = useUserQuery();
+  const {
+    data: userData,
+    isError: userError,
+    isLoading: userLoading,
+  } = useUserQuery();
   const { setIsOpen, setIsClose, key, isOpen } = useModal();
   const [isApplied, setIsApplied] = useState(false);
+  const [recentPosts, setRecentPosts] = useState([]);
+
+  useEffect(() => {
+    const recentPostsFromLocalStorage = localStorage.getItem('recentPosts');
+    if (recentPostsFromLocalStorage !== null) {
+      const recentPostsParsed = JSON.parse(recentPostsFromLocalStorage);
+      const sortedRecentPosts = recentPostsParsed.sort(
+        (a: any, b: any) => b.timestamp - a.timestamp,
+      );
+      const recentPostsToShow = sortedRecentPosts.slice(0, 6);
+      setRecentPosts(recentPostsToShow);
+    }
+  }, []);
 
   const handleApplyClick = () => {
-    if (!data?.data.item.phone) {
+    if (!userData?.data.item.phone) {
       setIsOpen('profileAlert');
     } else {
       setIsOpen('applySuccess');
@@ -59,7 +74,8 @@ const NoticeDetail = () => {
   const modalHeader =
     key === 'profileAlert' ? (
       <>
-        <Image src={cautionImg} alt="경고 표시" />내 프로필을 먼저 등록해 주세요.
+        <Image src={cautionImg} alt="경고 표시" />내 프로필을 먼저 등록해
+        주세요.
       </>
     ) : key === 'applySuccess' ? (
       <>
@@ -68,13 +84,17 @@ const NoticeDetail = () => {
       </>
     ) : null;
 
+  if (userLoading || !shop_id) {
+    return <p>Loading...</p>;
+  }
+
   return (
     <>
       <Header />
       <S.PageLayout>
         <S.TextWrap>
           <S.SmallText>식당</S.SmallText>
-          <S.BigText>도토리식당</S.BigText>
+          <S.BigText>{storeData?.name}</S.BigText>
         </S.TextWrap>
 
         <S.ContextWrap>
@@ -85,7 +105,8 @@ const NoticeDetail = () => {
           <S.TextContainer>
             <S.SmallText>시급</S.SmallText>
             <S.PriceWrap>
-              <PostPrice status="active" price={15000} priceChange={50} />
+            {/* priceChange 계산 */}
+              <PostPrice status="active" price={storeData?.originalHourlyPay} priceChange={50} /> 
             </S.PriceWrap>
             <S.WidgetWrap>
               <PostInform
@@ -95,12 +116,7 @@ const NoticeDetail = () => {
               />
               <PostInform status="active" type="장소" content="서울시 송파구" />
             </S.WidgetWrap>
-            <S.DetailText>
-              <p>알바하기 편한 너구리네 라면집!</p>
-              <p>
-                라면 올려두고 끓이기만 하면 되어서 쉬운 편에 속하는 가게입니다.
-              </p>
-            </S.DetailText>
+            <S.DetailText><p>{storeData?.description}</p></S.DetailText>
             {isApplied ? (
               <div style={{ width: '346px' }}>
                 <CustomButton
@@ -129,9 +145,9 @@ const NoticeDetail = () => {
         <S.RecentWrap>
           <S.BigText>최근에 본 공고</S.BigText>
           <S.PostContainer>
-            {/* {recentPosts.map((post, index) => (
+            {recentPosts.map((post, index) => (
               <Post key={index} {...post} />
-            ))} */}
+            ))}
           </S.PostContainer>
         </S.RecentWrap>
       </S.PageLayout>
@@ -170,10 +186,7 @@ const NoticeDetail = () => {
                 />
               </div>
               <div style={{ width: '80px' }}>
-                <RedButton 
-                  onClick={handleConfirmCancel} 
-                  text="취소하기" 
-                />
+                <RedButton onClick={handleConfirmCancel} text="취소하기" />
               </div>
             </>
           }
