20c8317bbe3bda167dcdaa1dcec72da96e4294ae
Kimhansaem <khs_38@naver.com>
2024-06-11 19:10:31 +0900

    feat: react-spinners 추가

--- src/pages/mypage/index.tsx
+++ src/pages/mypage/index.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import * as S from './index.styled';
 import { AddProfile } from '@/widgets/mypage/ui/AddProfile/AddProfile';
 import Header from '@/shared/components/Header/Header';
@@ -6,8 +6,15 @@ import Footer from '@/shared/components/Footer/Footer';
 import { useUserQuery } from '@/models/user/useUserData';
 import { ViewProfile } from '@/widgets/mypage/ui/ViewProfile/ViewProfile';
 import { EmptyList } from '@/widgets/mypage/ui/EmptyList/EmptyList';
+import { ClipLoader } from 'react-spinners';
+import { useUserData } from '@/shared/store/useUserData';
+import { useRouter } from 'next/router';
 
 const MyPage = () => {
+  const { type } = useUserData();
+  const router = useRouter();
+  if (type === 'employer') router.push('/mystore');
+
   const { data, isError, isLoading } = useUserQuery();
   const userData = data?.data.item;
   const content = data?.data?.item?.phone ? (
@@ -16,7 +23,6 @@ const MyPage = () => {
     <AddProfile />
   );
 
-  console.log(data);
   return (
     <>
       <Header />
@@ -26,7 +32,11 @@ const MyPage = () => {
             <S.ContentWrap>
               <S.ViewFlexBox>
                 <S.MyPageHeader>내 프로필</S.MyPageHeader>
-                {content}
+                {!isLoading ? (
+                  content
+                ) : (
+                  <ClipLoader size={50} color="#36d7b7" />
+                )}
               </S.ViewFlexBox>
             </S.ContentWrap>
           </S.Container>
@@ -37,7 +47,7 @@ const MyPage = () => {
           <S.Container>
             <S.ContentWrap>
               <S.MyPageHeader>내 프로필</S.MyPageHeader>
-              {content}
+              {!isLoading ? content : <ClipLoader size={50} color="#EA3C12" />}
             </S.ContentWrap>
           </S.Container>
         </>
