b64963a4458c1a31d070fd56a69d58250250b088
Kimhansaem <khs_38@naver.com>
2024-06-12 16:33:57 +0900

    feat: 파일 구조 변경

--- src/pages/mypage/index.tsx
+++ src/pages/mypage/index.tsx
@@ -1,53 +1,26 @@
-import React, { useEffect } from 'react';
-import * as S from './index.styled';
-import { AddProfile } from '@/widgets/mypage/ui/AddProfile/AddProfile';
+import React from 'react';
 import Header from '@/shared/components/Header/Header';
 import Footer from '@/shared/components/Footer/Footer';
-import { useUserQuery } from '@/models/user/useUserData';
-import { ViewProfile } from '@/widgets/mypage/ui/ViewProfile/ViewProfile';
-import { EmptyList } from '@/widgets/mypage/ui/EmptyList/EmptyList';
+
 import { useUserData } from '@/shared/store/useUserData';
 import { useRouter } from 'next/router';
-import { renderSpinner } from '@/shared/utils/renderSpinner';
+import { ProfileContent } from '@/widgets/mypage/ui/ProfileContent/ProfileContent';
+import { ApplicationContent } from '@/widgets/mypage';
+import { useUserQuery } from '@/models/user/useUserData';
 
 const MyPage = () => {
-  const { type } = useUserData();
   const router = useRouter();
+  const { type } = useUserData();
   if (type === 'employer') router.push('/mystore');
 
   const { data, isError, isLoading } = useUserQuery();
   const userData = data?.data.item;
-  const content = data?.data?.item?.phone ? (
-    <ViewProfile userData={userData} />
-  ) : (
-    <AddProfile />
-  );
 
   return (
     <>
       <Header />
-      {content.type.name === 'ViewProfile' ? (
-        <>
-          <S.Container>
-            <S.ContentWrap>
-              <S.ViewFlexBox>
-                <S.MyPageHeader>내 프로필</S.MyPageHeader>
-                {renderSpinner(content, isLoading)}
-              </S.ViewFlexBox>
-            </S.ContentWrap>
-          </S.Container>
-          <EmptyList />
-        </>
-      ) : (
-        <>
-          <S.Container>
-            <S.ContentWrap>
-              <S.MyPageHeader>내 프로필</S.MyPageHeader>
-              {renderSpinner(content, isLoading)}
-            </S.ContentWrap>
-          </S.Container>
-        </>
-      )}
+      <ProfileContent userData={userData} isLoading={isLoading} />
+      <ApplicationContent userData={userData} isLoading={isLoading} />
       <Footer />
     </>
   );
---
+++ src/widgets/mypage/ui/ProfileContent/ProfileContent.tsx
@@ -0,0 +1,43 @@
+import { useUserQuery } from '@/models/user/useUserData';
+import * as S from './ProfileContent.styled';
+import { renderSpinner } from '@/shared/utils/renderSpinner';
+import { AddProfile, ViewProfile } from '@/components/user';
+
+interface Props {
+  userData: UserDataType;
+  isLoading: boolean;
+}
+
+export const ProfileContent = ({ userData, isLoading }: Props) => {
+  const content = userData?.phone ? (
+    <ViewProfile userData={userData} />
+  ) : (
+    <AddProfile />
+  );
+
+  return (
+    <>
+      {content.type.name === 'ViewProfile' ? (
+        <>
+          <S.Container>
+            <S.ContentWrap>
+              <S.ViewFlexBox>
+                <S.MyPageHeader>내 프로필</S.MyPageHeader>
+                {renderSpinner(content, isLoading)}
+              </S.ViewFlexBox>
+            </S.ContentWrap>
+          </S.Container>
+        </>
+      ) : (
+        <>
+          <S.Container>
+            <S.ContentWrap>
+              <S.MyPageHeader>내 프로필</S.MyPageHeader>
+              {renderSpinner(content, isLoading)}
+            </S.ContentWrap>
+          </S.Container>
+        </>
+      )}
+    </>
+  );
+};
