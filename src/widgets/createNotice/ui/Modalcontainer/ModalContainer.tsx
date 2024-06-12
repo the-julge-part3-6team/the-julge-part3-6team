bc8dbd866a7f79b8bbaa8afc86eb18b536ae03ab
striker1826 <alstjq123579@daum.net>
2024-06-12 17:14:54 +0900

    refactor: 공고등록 페이지 리펙토링

---
+++ src/widgets/createNotice/ui/Modalcontainer/ModalContainer.tsx
@@ -0,0 +1,28 @@
+import * as S from './ModalContainer.styled';
+import PrimaryButton from '@/shared/components/Button/RedButton/RedButton';
+import Modal from '@/shared/components/Modal/Modal';
+import { useModal } from '@/shared/store/useModal';
+import { useRouter } from 'next/router';
+
+export const ModalContainer = () => {
+  const router = useRouter();
+  const { setIsClose } = useModal();
+
+  return (
+    <Modal
+      modalKey="등록완료 모달"
+      modalHeader={<S.ModalHeader>등록이 완료되었습니다.</S.ModalHeader>}
+      modalFooter={
+        <S.ModalFooter>
+          <PrimaryButton
+            text="확인"
+            onClick={() => {
+              setIsClose();
+              router.push('/mystore');
+            }}
+          />
+        </S.ModalFooter>
+      }
+    />
+  );
+};
--- src/widgets/createNotice/ui/inputContent/InputContainer.tsx
+++ src/widgets/createNotice/ui/inputContent/InputContainer.tsx
@@ -1,85 +1,102 @@
 import Input from '@/shared/components/Input/Input';
 import * as S from './InputContent.styled';
 import { Textarea } from '@/shared/components/Textarea/Textarea';
-import { ChangeEvent, Dispatch, SetStateAction } from 'react';
+import { ChangeEvent, useState } from 'react';
+import {
+  NOTICE_FORM_ERRORS_INITIAL_VALUE,
+  NOTICE_FORM_INITIAL_VALUE,
+} from '@/constant/notice';
+import { onSubmit } from '@/models/notice/noticeSubmit';
+import PrimaryButton from '@/shared/components/Button/RedButton/RedButton';
 
 interface Props {
-  notice: CreateNotice;
-  setNotice: any;
-  errors: any;
+  mutate: any;
 }
 
-export const InputContent = ({ notice, errors, setNotice }: Props) => {
+export const InputContent = ({ mutate }: Props) => {
+  const [notice, setNotice] = useState(NOTICE_FORM_INITIAL_VALUE);
+  const [errors, setErrors] = useState(NOTICE_FORM_ERRORS_INITIAL_VALUE);
+
   return (
-    <S.Layout>
-      <Input
-        label="시급*"
-        id="hourlyPay"
-        type="hourlyWage"
-        error={errors.hourlyPay}
-        value={String(notice.hourlyPay)}
-        placeholder="0"
-        inputType="text"
-        onChange={(e: ChangeEvent<HTMLInputElement>) =>
-          setNotice((prev: CreateNotice) => {
-            return {
-              ...prev,
-              ['hourlyPay']: e.target.value,
-            };
-          })
-        }
-      />
-      <Input
-        label="시작 일시*"
-        id="startsAt"
-        type="basic"
-        error={errors.startsAt}
-        value={notice.startsAt}
-        placeholder="2024-01-05 15:00"
-        inputType="datetime-local"
-        onChange={(e: ChangeEvent<HTMLInputElement>) =>
-          setNotice((prev: CreateNotice) => {
-            return {
-              ...prev,
-              ['startsAt']: e.target.value,
-            };
-          })
-        }
-      />
-      <Input
-        label="업무 시간*"
-        id="workHour"
-        type="basic"
-        error={errors.workhour}
-        value={String(notice.workhour)}
-        placeholder="0"
-        inputType="text"
-        onChange={(e: ChangeEvent<HTMLInputElement>) =>
-          setNotice((prev: CreateNotice) => {
-            return {
-              ...prev,
-              ['workhour']: e.target.value,
-            };
-          })
-        }
-      />
-      <S.Description>
-        <Textarea
-          id="description"
-          name="description"
-          placeholder="입력"
-          label="공고 설명"
-          value={notice.description}
-          onChange={(e: ChangeEvent<HTMLTextAreaElement>) =>
+    <>
+      <S.Layout>
+        <Input
+          label="시급*"
+          id="hourlyPay"
+          type="hourlyWage"
+          error={errors.hourlyPay}
+          value={String(notice.hourlyPay)}
+          placeholder="0"
+          inputType="text"
+          onChange={(e: ChangeEvent<HTMLInputElement>) =>
             setNotice((prev: CreateNotice) => {
               return {
                 ...prev,
-                ['description']: e.target.value,
+                ['hourlyPay']: Number(e.target.value),
               };
             })
           }
         />
-      </S.Description>
-    </S.Layout>
+        <Input
+          label="시작 일시*"
+          id="startsAt"
+          type="basic"
+          error={errors.startsAt}
+          value={notice.startsAt}
+          placeholder="2024-01-05 15:00"
+          inputType="datetime-local"
+          onChange={(e: ChangeEvent<HTMLInputElement>) =>
+            setNotice((prev: CreateNotice) => {
+              return {
+                ...prev,
+                ['startsAt']: e.target.value,
+              };
+            })
+          }
+        />
+        <Input
+          label="업무 시간*"
+          id="workHour"
+          type="basic"
+          error={errors.workhour}
+          value={String(notice.workhour)}
+          placeholder="0"
+          inputType="text"
+          onChange={(e: ChangeEvent<HTMLInputElement>) =>
+            setNotice((prev: CreateNotice) => {
+              return {
+                ...prev,
+                ['workhour']: Number(e.target.value),
+              };
+            })
+          }
+        />
+        <S.Description>
+          <Textarea
+            id="description"
+            name="description"
+            placeholder="입력"
+            label="공고 설명"
+            value={notice.description}
+            onChange={(e: ChangeEvent<HTMLTextAreaElement>) =>
+              setNotice((prev: CreateNotice) => {
+                return {
+                  ...prev,
+                  ['description']: e.target.value,
+                };
+              })
+            }
+          />
+        </S.Description>
+      </S.Layout>
+      <S.ButtonCotainer>
+        <S.ButtonWrap>
+          <PrimaryButton
+            text="등록하기"
+            onClick={() => onSubmit(notice, setErrors, mutate)}
+          />
+        </S.ButtonWrap>
+      </S.ButtonCotainer>
+    </>
   );
 };
