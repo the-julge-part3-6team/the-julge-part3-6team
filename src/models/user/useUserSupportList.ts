7a15dd82e5402332d456d81b58311d7da441ac19
Kimhansaem <khs_38@naver.com>
2024-06-14 20:09:56 +0900

    feat: mypage > 테이블 페이지 네이션 추가

--- src/models/user/useUserSupportList.ts
+++ src/models/user/useUserSupportList.ts
@@ -2,13 +2,21 @@ import { useUserData } from '@/shared/store/useUserData';
 import { apiInstance } from '@/shared/utils/axios';
 import { useQuery } from '@tanstack/react-query';
 
-export const useUserSupportList = () => {
+export const useUserSupportList = ({
+  limit,
+  offset,
+}: {
+  limit: number;
+  offset: number;
+}) => {
   const { user_id } = useUserData();
 
   const { data, isError, isLoading } = useQuery({
-    queryKey: [`/users/${user_id}/applications`],
+    queryKey: [`/users/${user_id}/applications`, limit, offset],
     queryFn: () => {
-      return apiInstance.get(`/users/${user_id}/applications`);
+      return apiInstance.get(
+        `/users/${user_id}/applications?offset=${offset}&limit=${limit}`,
+      );
     },
     enabled: !!user_id,
   });
