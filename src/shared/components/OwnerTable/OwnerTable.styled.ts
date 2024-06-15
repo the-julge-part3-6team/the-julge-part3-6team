15df4e292802bba329453b1f78afa000d58c93ab
Kimhansaem <khs_38@naver.com>
2024-06-15 17:06:07 +0900

    feat: noticedetailceo > table /// 승인 거절 버튼 추가 등 기능 추가

--- src/shared/components/OwnerTable/OwnerTable.styled.ts
+++ src/shared/components/OwnerTable/OwnerTable.styled.ts
@@ -3,6 +3,8 @@ import styled from 'styled-components';
 
 export const CustomTable = styled.div`
   width: 100%;
+  background: ${theme.Colors.White};
+  min-height: 338px;
   border-radius: 10px;
 
   @media (max-width: 744px) {
@@ -42,6 +44,8 @@ export const CustomTableHeader = styled.ul`
 export const CustomTableBody = styled.ul`
   display: grid;
   grid-template-columns: 23.5% 31% 21% 24.5%;
+  align-items: center;
+
   border-bottom: 1px solid ${theme.Colors.Gray[20]};
   font-size: 16px;
   background-color: ${theme.Colors.White};
@@ -78,4 +82,10 @@ export const CustomTableWrap = styled.div`
   @media (max-width: 744px) {
     width: 200%;
   }
+`;
+
+export const TableBtnWrap = styled.div`
+  width: 100%;
+  display: flex;
+  gap: 12px;
 `;
